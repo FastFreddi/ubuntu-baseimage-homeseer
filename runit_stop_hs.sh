a248734d1c8f100a7dd5b0442c0f29cb0cd6774a
FastFreddi <flpaulin@evilnet.ca>
2021-01-11 05:46:41 -0330

    Update and rename stop_homeseer.sh to runit_stop_hs.sh

---
+++ runit_stop_hs.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /usr/local/HomeSeer
+./hs_stop.sh
--- stop_homeseer.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-/usr/bin/curl 'http://127.0.0.1/LinuxTools' -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' --data 'ConfirmShutdownhs=Yes' --compressed
-sleep 10s
